58e04a7ba223cbdd542696a6a745d428593cf993
fncreative <dann.frey@me.com>
2018-04-16 12:36:10 -0700

    Week 2 Assignment

---
+++ two/index.js
@@ -0,0 +1,54 @@
+"use strict";
+
+var http = require("http"), fs = require('fs'), qs = require("querystring"), records = require("./lib/records");
+
+
+
+http.createServer(function(req,res){
+  console.log("url = " + req.url);
+  
+  var url = req.url.split("?");
+  var params = qs.parse(url[1]);
+  console.log(params);
+  console.log(typeof params);
+  let found = records.get(params.name);
+  var path = url[0].toLowerCase();
+  switch(path) {
+    case '/': 
+      fs.readFile(__dirname + '/home.html', function(err, data){
+            if (err) {
+            res.writeHead(500, {'Content-Type': 'text/plain'});
+            res.end('500 - internal error');
+            } else{
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(data);
+            }
+          });
+      break;
+    case '/about':
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end('About');
+      break;
+    case '/search':
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end('Record Results: ' + params.name + "\n" + JSON.stringify(found));
+      break;
+    case '/add':
+      records.add(params);
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end('Record Added' + '\n' + 'Title: ' + params.name + '\n' + 'Year: ' + params.year + '\n' + 'Label: ' + params.label);
+      break;
+    case '/delete':
+      var deleted = records.delete(params.name);
+      res.end('Record Deleted' + '\n' + 'Title: ' + params.name + '\n' + 'Total Records Remaining:' +  deleted.Total);
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      break;
+    case '/all':
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end(JSON.stringify(records.allRecords()));
+    default:
+      res.writeHead(404, {'Content-Type': 'text/plain'});
+      res.end('404:Page not found.');
+  }
+  
+}).listen(process.env.PORT || 3000);
---
+++ two/lib/records.js
@@ -0,0 +1,43 @@
+"use strict";
+
+let records = [
+    {name : "Under the Table and Dreaming", year : 1994, label : "RCA"},
+    {name : "Crash", year : 1996, label : "RCA"},
+    {name : "Before These Crowded Streets", year : 1998, label : "RCA"},
+    {name : "Everyday", year : 2001, label : "RCA"},
+    {name : "Busted Stuff", year : 2001, label : "RCA"},
+    {name : "Stand Up", year : 2005, label : "RCA"},
+    {name : "Big Whiskey & The GrooGrux King", year : 2009, label : "RCA"},
+    {name : "Away from the World", year : 2009, label : "RCA"},
+    ];
+    
+    //console.log(records);
+    
+    
+    exports.get = (name) => {
+        return records.find((item) => {
+            return item.name == name;
+        });
+    };
+    
+    exports.delete = (name) => {
+        var recordLen = records.length;
+        records = records.filter((item) => {
+            return item.name !== name;
+        });
+        var deleted = (records.length == recordLen) ? "" : "deleted";
+        return { "RecordAction": deleted, "Total": records.length};
+    };
+    
+    exports.add = (newRecord) => {
+        var recordName = newRecord.name;
+        var recordYear = newRecord.year;
+        var recordLabel = newRecord.label;
+        var completeRecord = {name : recordName, year : recordYear, label : recordLabel};
+        records.push(completeRecord);
+    };
+    
+    exports.allRecords = () => {
+        return records;
+    };
+    
