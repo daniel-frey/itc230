b8ba353c622baa235d2471cb9d9431972db3cadc
Daniel Frey <dann.frey@me.com>
2018-05-18 10:08:05 -0700

    initial week 5

---
+++ five/index.js
@@ -0,0 +1,55 @@
+"use strict";
+
+let record = require("./lib/records.js");
+
+const express = require('express');
+
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.use(express.static(__dirname + '/public')); // allows direct navigation to static files
+app.use(require("body-parser").urlencoded({extended: true}));
+
+let handlebars =  require("express-handlebars");
+app.engine('.html', handlebars({extname: '.html'}));
+app.set("view engine", '.html');
+
+//app.get is the method to add routes app.VERB
+app.get('/', function(req, res){
+  res.type('text/html');
+  res.sendFile(__dirname + '/public/home.html');
+});
+
+app.get('/about', function(req, res){
+  res.type('text/plain');
+  res.send('About Page');
+});
+
+app.get('/delete', function(req, res){
+  let deleted = record.delete(req.query.name);
+  res.render('delete', {name: req.query.name, result: deleted});
+});
+
+app.post('/get', function(req, res){
+  var header = "Searching for: " + req.body.name + ' by David Bowie<br>';
+  var match = record.get(req.body.name);
+  res.render("details", {name: req.body.name, result: match});
+});
+
+app.post('/add', function(req, res){
+  let completeRecord = {name : req.body.name, year : req.body.year, label : req.body.label};
+  let added = record.add(completeRecord);
+  res.render('add',{name: req.body.name, result: added});
+});
+
+//404 page
+app.use(function(req, res){
+  res.status(404);
+  res.render('404');
+});
+
+
+app.listen(app.get('port'), function(){
+  console.log('Express has started on http://localhost: ' +
+    app.get('port') + ';press Ctrl-C to terminate.');
+});
