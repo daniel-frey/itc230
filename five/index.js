7343963282c2f259aa6bb959b5d9f54131a3b6b9
Daniel Frey <dann.frey@me.com>
2018-05-24 15:00:11 -0700

    fixes

--- five/index.js
+++ five/index.js
@@ -15,31 +15,12 @@ app.engine('.html', handlebars({extname: '.html'}));
 app.set("view engine", '.html');
 
 //app.get is the method to add routes app.VERB
-app.get('/', function(req, res){
-  res.type('text/html');
-  res.sendFile(__dirname + '/public/home.html');
-});
-
-app.get('/about', function(req, res){
-  res.type('text/plain');
-  res.send('About Page');
-});
-
-app.get('/delete', function(req, res){
-  let deleted = record.delete(req.query.name);
-  res.render('delete', {name: req.query.name, result: deleted});
-});
-
-app.post('/get', function(req, res){
-  var header = "Searching for: " + req.body.name + ' by David Bowie<br>';
-  var match = record.get(req.body.name);
-  res.render("details", {name: req.body.name, result: match});
-});
-
-app.post('/add', function(req, res){
-  let completeRecord = {name : req.body.name, year : req.body.year, label : req.body.label};
-  let added = record.add(completeRecord);
-  res.render('add',{name: req.body.name, result: added});
+app.get('/', (req, res, next) => {
+  record.find({}, function (err, items) {
+    if (err) return next(err);
+    console.log(items.length);
+    res.render('home', {records: items }); 
+  });
 });
 
 //404 page
@@ -47,7 +28,6 @@ app.use(function(req, res){
   res.status(404);
   res.render('404');
 });
-
 
 app.listen(app.get('port'), function(){
   console.log('Express has started on http://localhost: ' +
--- five/lib/records.js
+++ five/lib/records.js
@@ -3,7 +3,7 @@
 let records = require("../models/Record.js");
 
 // return all records
-Record.find({}, (err, items) => {
+records.find({}, (err, items) => {
     if (err) return next(err);
     console.log(items.length);
     // other code here
